c3edf55fa16d488761d04715ba56faf41e299b1d
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2022-05-04 14:34:55 +0800

    gitignore update
    stupid script to deploy 10 dogies at a time
    
    added metadata dir to gitignore
    Im not uploading all of those files

--- scripts/advanced_collectible/deploy_10_nft.py
+++ scripts/advanced_collectible/deploy_10_nft.py
@@ -2,11 +2,14 @@ from scripts.advanced_collectible.create_collectible import create_collectible
 
 
 def deploy10():
-    tx = create_collectible()
-    tx = create_collectible()
-    tx = create_collectible()
-    tx = create_collectible()
-
+    toDeploy = 3
+    for n in range(toDeploy):
+        print(f"run number {(n + 1)}")
+        create_collectible()
+        if n > toDeploy:
+            print("done, execute next")
+        else:
+            print("job finished")
 
 def main():
     deploy10()
